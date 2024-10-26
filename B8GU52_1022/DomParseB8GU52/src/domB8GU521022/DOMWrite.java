9604e415535a035557c1c701aa62203f15f1f66a
KolaSandor <100164779+KolaSandor@users.noreply.github.com>
2024-10-26 20:39:16 +0200

    Add files via upload

--- B8GU52_1022/DomParseB8GU52/src/domB8GU521022/DOMWrite.java
+++ B8GU52_1022/DomParseB8GU52/src/domB8GU521022/DOMWrite.java
@@ -18,5 +18,59 @@ import org.w3c.dom.Node;
 
 public class DOMWrite {
 	
+	public static void main(String[] args) throws PaerserConfigurationException, TransformerException {
+	
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder builder = factory.newDocumentBuilder();
+		
+		Document doc = builder.newDocument();
+		
+		Element root = doc.createElementNS("DOMB8GU52" , "hallgatok");
+		doc.appendChild(root);
+		
+		root.appendChild(createUser(doc, "1", "Peter", "Nagy", "Web Developer"));
+		root.appendChild(createUser(doc, "2", "Piroska", "Vigh", "Java Programozo"));
+		root.appendChild(createUser(doc, "3", "Ferenc", "Kiss", "Associate Professor"));
+		
+		TransformerFactory transformerFactory = TransformerFactory.newInstance();
+		Transformer transf = transformerFactory.newTransformer();
+		
+		DOMSource source = new DOMSource(doc);
+		
+		File myFile = new File("B8GU52.xml");
+		
+		StreamResult console = new StreamResult(System.out);
+		StreamResult file = new StreamResult(myFile);
+		
+		transf.transform(source, console);
+		transf.transform(source, file);
+
+		
+	}
+	
+	
+
+	private static Node createUser(Document doc, String id, String firstName, String lastName, String profession) {
+		
+		Element user = doc.createElement("hallgato");
+		
+		user.setAttribute("id", id );
+		user.appendChild(createUserElement(doc, "keresztnev", firstName));
+		user.appendChild(createUserElement(doc, "vezeteknev", lastName));
+		user.appendChild(createUserElement(doc, "keresztnev", profession));
+		return user;
+		
+	}
+
+
+
+	private static Node createUserElement(Document doc, String name, String value) {
+		
+		
+		Element node = doc.createElement(name);
+		node.appendChild(doc.createTextNode(value));
+		return node;
+		
+	}
 	
 }
