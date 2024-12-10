eb5e01fc37ca018cc04f28ca15ab4cd79f7d4695
KolaSandor <100164779+KolaSandor@users.noreply.github.com>
2024-12-10 14:26:40 +0100

    Add files via upload

--- XMLTaskB8GU52/DomParseB8GU52/src/DomParseB8GU52/DOMReadB8GU52.java
+++ XMLTaskB8GU52/DomParseB8GU52/src/DomParseB8GU52/DOMReadB8GU52.java
@@ -1,54 +1,110 @@
 package DomParseB8GU52;
 
-import org.w3c.dom.*;
-import javax.xml.parsers.*;
 import java.io.File;
+import java.io.IOException;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
 
 public class DOMReadB8GU52 {
-    public static void main(String[] args) {
-        try {
-            File xmlFile = new File("XMLB8GU52.xml");
-            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-            Document doc = dBuilder.parse(xmlFile);
-            doc.getDocumentElement().normalize();
+    public static void main(String[] args) throws SAXException, IOException, ParserConfigurationException {
 
-            System.out.println("Root element: " + doc.getDocumentElement().getNodeName());
-            printElementContent(doc.getDocumentElement(), "");
+        File xmlFile = new File("XMLB8GU52.xml");
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder = factory.newDocumentBuilder();
+        Document doc = dBuilder.parse(xmlFile);
 
-    public static void printElementContent(Node node, String indent) {
-        if (node.getNodeType() == Node.ELEMENT_NODE) {
-            // Kiírja az aktuális elem nevét
-            System.out.println(indent + "Element: " + node.getNodeName());
+        doc.getDocumentElement().normalize();
 
-            // Kiírja az attribútumokat (ha vannak)
-            if (node.hasAttributes()) {
-                NamedNodeMap attributes = node.getAttributes();
-                for (int i = 0; i < attributes.getLength(); i++) {
-                    Node attribute = attributes.item(i);
-                    System.out.println(indent + "  Attribute - " + attribute.getNodeName() + ": " + attribute.getNodeValue());
-                }
+        System.out.println("Gyökér elem: " + doc.getDocumentElement().getNodeName());
+
+        // Étterem elemek feldolgozása
+        NodeList ettermek = doc.getElementsByTagName("Étterem");
+        for (int i = 0; i < ettermek.getLength(); i++) {
+            Node etteremNode = ettermek.item(i);
+            if (etteremNode.getNodeType() == Node.ELEMENT_NODE) {
+                Element etteremElem = (Element) etteremNode;
+                System.out.println("\nÉtterem ID: " + etteremElem.getAttribute("id"));
+                System.out.println("Név: " + etteremElem.getElementsByTagName("Név").item(0).getTextContent());
+                System.out.println("Telefonszám: " + etteremElem.getElementsByTagName("Telefonszám").item(0).getTextContent());
+                Element cimElem = (Element) etteremElem.getElementsByTagName("Cím").item(0);
+                System.out.println("Cím: " +
+                        cimElem.getElementsByTagName("Irányítószám").item(0).getTextContent() + ", " +
+                        cimElem.getElementsByTagName("Utca").item(0).getTextContent() + " " +
+                        cimElem.getElementsByTagName("Házszám").item(0).getTextContent());
             }
+        }
 
-            // Gyermekelemek feldolgozása
-            NodeList children = node.getChildNodes();
-            for (int i = 0; i < children.getLength(); i++) {
-                Node child = children.item(i);
+        // Alkalmazott elemek feldolgozása
+        NodeList alkalmazottak = doc.getElementsByTagName("Alkalmazott");
+        for (int i = 0; i < alkalmazottak.getLength(); i++) {
+            Node alkalmazottNode = alkalmazottak.item(i);
+            if (alkalmazottNode.getNodeType() == Node.ELEMENT_NODE) {
+                Element alkalmazottElem = (Element) alkalmazottNode;
+                System.out.println("\nAlkalmazott ID: " + alkalmazottElem.getAttribute("id"));
+                System.out.println("Munkahely: " + alkalmazottElem.getAttribute("Munka"));
+                System.out.println("Név: " + alkalmazottElem.getElementsByTagName("Név").item(0).getTextContent());
+                System.out.println("TAJ: " + alkalmazottElem.getElementsByTagName("TAJ").item(0).getTextContent());
+                System.out.println("Bankszámlaszám: " + alkalmazottElem.getElementsByTagName("Bankszámlaszám").item(0).getTextContent());
+                System.out.println("Születési Idő: " + alkalmazottElem.getElementsByTagName("SzületésiIdő").item(0).getTextContent());
+                System.out.println("Beosztás: " + alkalmazottElem.getElementsByTagName("Beosztás").item(0).getTextContent());
+            }
+        }
 
-                if (child.getNodeType() == Node.ELEMENT_NODE) {
-                    printElementContent(child, indent + "  ");
-                } else if (child.getNodeType() == Node.TEXT_NODE) {
-                    String textContent = child.getTextContent().trim();
-                    if (!textContent.isEmpty()) {
-                        System.out.println(indent + "  Text: " + textContent);
+        // Rendelés elemek feldolgozása
+        NodeList rendelesek = doc.getElementsByTagName("Rendelés");
+        for (int i = 0; i < rendelesek.getLength(); i++) {
+            Node rendelesNode = rendelesek.item(i);
+            if (rendelesNode.getNodeType() == Node.ELEMENT_NODE) {
+                Element rendelesElem = (Element) rendelesNode;
+                System.out.println("\nRendelés ID: " + rendelesElem.getAttribute("id"));
+                System.out.println("Felszolgálva: " + rendelesElem.getAttribute("Felszolgálva"));
+                System.out.println("Rendelve: " + rendelesElem.getAttribute("Rendelve"));
+                System.out.println("Ár: " + rendelesElem.getElementsByTagName("Ár").item(0).getTextContent());
+                System.out.println("Idő: " + rendelesElem.getElementsByTagName("Idő").item(0).getTextContent());
+                System.out.println("Státusz: " + rendelesElem.getElementsByTagName("Státusz").item(0).getTextContent());
+            }
+        }
+
+     // Termék elemek feldolgozása
+        NodeList termekekList = doc.getElementsByTagName("Termék");
+        for (int i = 0; i < termekekList.getLength(); i++) {
+            Node termekNode = termekekList.item(i);
+            if (termekNode.getNodeType() == Node.ELEMENT_NODE) {
+                Element termekElem = (Element) termekNode;
+                System.out.println("\nTermék ID: " + termekElem.getAttribute("id"));
+                System.out.println("Név: " + termekElem.getElementsByTagName("Név").item(0).getTextContent());
+                System.out.println("Típus: " + termekElem.getElementsByTagName("Típus").item(0).getTextContent());
+
+                // Alapanyagok feldolgozása
+                NodeList alapanyagok = termekElem.getElementsByTagName("Alapanyag");
+                System.out.print("Alapanyagok: ");
+                for (int j = 0; j < alapanyagok.getLength(); j++) {
+                    System.out.print(alapanyagok.item(j).getTextContent());
+                    if (j < alapanyagok.getLength() - 1) {
+                        System.out.print(", ");
                     }
                 }
+                System.out.println();
             }
         }
+
+
+     // Vendég elemek feldolgozása
+        NodeList vendegList = doc.getElementsByTagName("Vendég");
+        for (int i = 0; i < vendegList.getLength(); i++) {
+            Node vendegNode = vendegList.item(i);
+            if (vendegNode.getNodeType() == Node.ELEMENT_NODE) {
+                Element vendegElem = (Element) vendegNode;
+                System.out.println("\nVendég ID: " + vendegElem.getAttribute("id"));
+                System.out.println("Név: " + vendegElem.getElementsByTagName("Név").item(0).getTextContent());
+                System.out.println("Email: " + vendegElem.getElementsByTagName("Email").item(0).getTextContent());
+                System.out.println("Telefonszám: " + vendegElem.getElementsByTagName("Telefonszám").item(0).getTextContent());
+            }
+        }
+
     }
 }
--- XMLTaskB8GU52/DomParseB8GU52/src/DomParseB8GU52/DOMWriteB8GU52.java
+++ XMLTaskB8GU52/DomParseB8GU52/src/DomParseB8GU52/DOMWriteB8GU52.java
@@ -1,62 +1,158 @@
 package DomParseB8GU52;
 
-import org.w3c.dom.*;
-import javax.xml.parsers.*;
-import javax.xml.transform.*;
+import java.io.File;
+import java.io.StringWriter;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
-import java.io.File;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 public class DOMWriteB8GU52 {
-    public static void main(String[] args) {
-        try {
-            File xmlFile = new File("XMLB8GU52.xml");
-            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-            Document doc = dBuilder.parse(xmlFile);
-            doc.getDocumentElement().normalize();
 
-            System.out.println("Writing XML tree structure:");
-            writeNode(doc.getDocumentElement(), "");
+    public static void main(String[] args) throws ParserConfigurationException, TransformerException {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document doc = builder.newDocument();
 
-            saveToFile(doc, "Output_XML_B8GU52.xml");
-            System.out.println("Saved to Output_XML_B8GU52.xml");
+        // Gyökérelem
+        Element root = doc.createElement("B8GU52_Étterem");
+        doc.appendChild(root);
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        // Éttermek hozzáadása
+        root.appendChild(createÉtterem(doc, "E001", "Gourmet Bistro", "06301234567", "12345", "Elm Avenue", "34"));
+        root.appendChild(createÉtterem(doc, "E002", "Pepper's Grill", "06309876543", "54321", "Oak Street", "12"));
+        root.appendChild(createÉtterem(doc, "E003", "Sunrise Café", "06701239876", "11223", "Willow Lane", "89"));
+
+        // Alkalmazottak hozzáadása
+        root.appendChild(createAlkalmazott(doc, "A001", "E001", "Emily Watson", "M789", "DEF987654", "1985-03-10", "Nem"));
+        root.appendChild(createAlkalmazott(doc, "A002", "E002", "Michael Brown", "M456", "DEF123456", "1992-11-20", "Igen"));
+        root.appendChild(createAlkalmazott(doc, "A003", "E003", "Sarah Johnson", "M789", "DEF678901", "2000-06-15", "Nem"));
+
+        // Rendelések hozzáadása
+        root.appendChild(createRendelés(doc, "R001", "E001", "V001", "3000", "2024.01.01", "Aktív"));
+        root.appendChild(createRendelés(doc, "R002", "E002", "V002", "12000", "2024.02.20", "Kiszállítva"));
+        root.appendChild(createRendelés(doc, "R003", "E003", "V003", "5800", "2024.03.15", "Kiszállítva"));
+
+        // Termékek hozzáadása
+        root.appendChild(createTermék(doc, "T001", "Deluxe Pizza", "Főétel", new String[]{"pepperoni", "mozzarella", "olívabogyó"}));
+        root.appendChild(createTermék(doc, "T002", "Veggie Delight", "Előétel", new String[]{"spenót", "paradicsom"}));
+        root.appendChild(createTermék(doc, "T003", "Tropical Salad", "Köret", new String[]{"saláta", "mangó", "uborka"}));
+
+        // Vendégek hozzáadása
+        root.appendChild(createVendég(doc, "V001", "Laura Smith", "laura@example.com", "06305556677"));
+        root.appendChild(createVendég(doc, "V002", "Daniel Carter", "daniel@example.com", "06701233445"));
+        root.appendChild(createVendég(doc, "V003", "Sophia White", "sophia@example.com", "06307778899"));
+
+        // Termékek-Rendelések kapcsolatok
+        root.appendChild(createTermékek(doc, "T001", "R001", "3200"));
+        root.appendChild(createTermékek(doc, "T002", "R002", "2800"));
+        root.appendChild(createTermékek(doc, "T003", "R003", "4500"));
+
+        // Konzolra kiírás
+        System.out.println("XML fa:");
+        printDocument(doc);
+
+        // XML mentése fájlba
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+
+        DOMSource source = new DOMSource(doc);
+        StreamResult result = new StreamResult(new File("Output_B8GU52.xml"));
+
+        transformer.transform(source, result);
+
+        System.out.println("XML létrehozva: Output_B8GU52.xml");
     }
 
-    public static void writeNode(Node node, String indent) {
-        if (node.getNodeType() == Node.ELEMENT_NODE) {
-            System.out.println(indent + node.getNodeName());
+    private static void printDocument(Document doc) throws TransformerException {
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "no");
 
-            if (node.hasAttributes()) {
-                NamedNodeMap attributes = node.getAttributes();
-                for (int i = 0; i < attributes.getLength(); i++) {
-                    Node attribute = attributes.item(i);
-                    System.out.println(indent + " - " + attribute.getNodeName() + ": " + attribute.getNodeValue());
-                }
-            }
+        StringWriter writer = new StringWriter();
+        StreamResult console = new StreamResult(writer);
+        DOMSource source = new DOMSource(doc);
 
-            NodeList children = node.getChildNodes();
-            for (int i = 0; i < children.getLength(); i++) {
-                writeNode(children.item(i), indent + "  ");
-            }
-        }
+        transformer.transform(source, console);
+        System.out.println(writer.toString());
     }
 
-    public static void saveToFile(Document doc, String filename) {
-        try {
-            TransformerFactory transformerFactory = TransformerFactory.newInstance();
-            Transformer transformer = transformerFactory.newTransformer();
-            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+    private static Element createÉtterem(Document doc, String id, String név, String telefonszám, String irsz, String utca, String házszám) {
+        Element étterem = doc.createElement("Étterem");
+        étterem.setAttribute("id", id);
+        étterem.appendChild(createElement(doc, "Név", név));
+        étterem.appendChild(createElement(doc, "Telefonszám", telefonszám));
+        Element cím = doc.createElement("Cím");
+        cím.appendChild(createElement(doc, "Irányítószám", irsz));
+        cím.appendChild(createElement(doc, "Utca", utca));
+        cím.appendChild(createElement(doc, "Házszám", házszám));
+        étterem.appendChild(cím);
+        return étterem;
+    }
 
-            DOMSource source = new DOMSource(doc);
-            StreamResult result = new StreamResult(new File(filename));
-            transformer.transform(source, result);
-        } catch (Exception e) {
-            e.printStackTrace();
+    private static Element createAlkalmazott(Document doc, String id, String munka, String név, String taj, String bankszámlaszám, String születésiIdő, String beosztás) {
+        Element alkalmazott = doc.createElement("Alkalmazott");
+        alkalmazott.setAttribute("id", id);
+        alkalmazott.setAttribute("Munka", munka);
+        alkalmazott.appendChild(createElement(doc, "Név", név));
+        alkalmazott.appendChild(createElement(doc, "TAJ", taj));
+        alkalmazott.appendChild(createElement(doc, "Bankszámlaszám", bankszámlaszám));
+        alkalmazott.appendChild(createElement(doc, "SzületésiIdő", születésiIdő));
+        alkalmazott.appendChild(createElement(doc, "Beosztás", beosztás));
+        return alkalmazott;
+    }
+
+    private static Element createRendelés(Document doc, String id, String felszolgálva, String rendelve, String ár, String idő, String státusz) {
+        Element rendelés = doc.createElement("Rendelés");
+        rendelés.setAttribute("id", id);
+        rendelés.setAttribute("Felszolgálva", felszolgálva);
+        rendelés.setAttribute("Rendelve", rendelve);
+        rendelés.appendChild(createElement(doc, "Ár", ár));
+        rendelés.appendChild(createElement(doc, "Idő", idő));
+        rendelés.appendChild(createElement(doc, "Státusz", státusz));
+        return rendelés;
+    }
+
+    private static Element createTermék(Document doc, String id, String név, String típus, String[] alapanyagok) {
+        Element termék = doc.createElement("Termék");
+        termék.setAttribute("id", id);
+        termék.appendChild(createElement(doc, "Név", név));
+        termék.appendChild(createElement(doc, "Típus", típus));
+        for (String alapanyag : alapanyagok) {
+            termék.appendChild(createElement(doc, "Alapanyag", alapanyag));
         }
+        return termék;
+    }
+
+    private static Element createVendég(Document doc, String id, String név, String email, String telefonszám) {
+        Element vendég = doc.createElement("Vendég");
+        vendég.setAttribute("id", id);
+        vendég.appendChild(createElement(doc, "Név", név));
+        vendég.appendChild(createElement(doc, "Email", email));
+        vendég.appendChild(createElement(doc, "Telefonszám", telefonszám));
+        return vendég;
+    }
+
+    private static Element createTermékek(Document doc, String termékId, String rendelésId, String ár) {
+        Element termékek = doc.createElement("Termékek");
+        termékek.setAttribute("termék_id", termékId);
+        termékek.setAttribute("rendelés_id", rendelésId);
+        termékek.appendChild(createElement(doc, "Ár", ár));
+        return termékek;
+    }
+
+    private static Element createElement(Document doc, String name, String value) {
+        Element element = doc.createElement(name);
+        element.appendChild(doc.createTextNode(value));
+        return element;
     }
 }
